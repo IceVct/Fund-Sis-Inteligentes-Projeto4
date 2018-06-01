b039da3832af6b347132a765257d601bee006306
alvarovieira <alvarotv06@hotmail.com>
2018-06-01 00:27:58 -0300

    codigo de frb

---
+++ projeto4_FRB.m
@@ -0,0 +1,42 @@
+%% Script principal do projeto 4
+% Alvaro Torres Vieira - 
+% Victor Araujo Vieira - 14/0032801
+
+close all;
+clear all;
+clc;
+
+%% Le os arquivos csv que contem os dados de treino e teste
+dados = readtable('./winequality-red.csv', 'Delimiter', ';');
+
+% A sintaxe para acessar os valores das tabelas geradas na leitura dos csv
+% eh, por exemplo: treino{1, 3}, acesssando terceira coluna da primeira
+% linha
+
+% pegando os valores das tabelas e gerando variaveis para serem passadas
+% nos modelos a serem treinados
+labels = dados{:, 12}';
+valores = dados{:, 1:11}';
+n = length(labels);
+
+%MLP
+%traincgb
+%traincgf
+% trainoss o melhor ate agora
+P = valores;
+T = labels;
+goal = 0.38;
+
+net = newrb(P,T,goal);
+net.divideFcn = 'divideint';
+net.trainFcn = 'traingd';
+[net, trainParameters] = train(net, valores, labels);
+
+% Validacoes
+testX = valores(:,trainParameters.testInd);
+testT = labels(:,trainParameters.testInd);
+testY = net(testX);
+testY = round(testY);
+performance = sum(testY == testT)/length(testT);
+geraMatConf(testT, testY, testX);
+
